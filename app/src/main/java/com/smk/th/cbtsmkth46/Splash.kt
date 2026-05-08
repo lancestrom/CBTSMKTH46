0cbd7700b5ee16a3d5ec2b7c9f9b9bf8867e6669
RAHMADIKA SETIAWAN <dikakewren@gmail.com>
2026-05-08 21:52:43 +0700

    update 2

---
+++ app/src/main/java/com/smk/th/cbtsmkth46/Splash.kt
@@ -0,0 +1,41 @@
+package com.smk.th.cbtsmkth46
+
+import android.content.Intent
+import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.WindowCompat
+import androidx.core.view.WindowInsetsCompat
+import androidx.core.view.WindowInsetsControllerCompat
+
+class Splash : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        
+        // Buat Splash Fullscreen Total
+        WindowCompat.setDecorFitsSystemWindows(window, false)
+        hideSystemUI()
+        
+        setContentView(R.layout.activity_splash)
+
+        // Tunggu 5 detik lalu pindah ke WebView
+        Handler(Looper.getMainLooper()).postDelayed({
+            val intent = Intent(this, WebView::class.java)
+            startActivity(intent)
+            finish()
+        }, 5000)
+    }
+
+    private fun hideSystemUI() {
+        WindowInsetsControllerCompat(window, window.decorView).apply {
+            hide(WindowInsetsCompat.Type.statusBars() or WindowInsetsCompat.Type.navigationBars())
+            systemBarsBehavior = WindowInsetsControllerCompat.BEHAVIOR_SHOW_TRANSIENT_BARS_BY_SWIPE
+        }
+    }
+
+    override fun onResume() {
+        super.onResume()
+        hideSystemUI()
+    }
+}
