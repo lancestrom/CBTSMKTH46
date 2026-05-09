9610335c183b9e41efb08a2dc65667e0131e8453
RAHMADIKA SETIAWAN <dikakewren@gmail.com>
2026-05-09 17:46:58 +0700

    DELET TOKEN MASUK

--- app/src/main/java/com/smk/th/cbtsmkth46/Splash.kt
+++ app/src/main/java/com/smk/th/cbtsmkth46/Splash.kt
@@ -4,6 +4,7 @@ import android.content.Intent
 import android.os.Bundle
 import android.os.Handler
 import android.os.Looper
+import android.webkit.WebView
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.WindowCompat
 import androidx.core.view.WindowInsetsCompat
